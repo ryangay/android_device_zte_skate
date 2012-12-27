a08ec843dfde79617289914a94d7115a4fed4c9b
Ryan Gay <ryan.sn.karoo@gmail.com>
2012-12-27 20:47:26 +0000

    Revert "skate: change to pac and 4.1.2"
    This reverts commit 605c43933aa65417f14d345ab1e83977bf4163d1.

--- cm.mk
+++ cm.mk
@@ -11,9 +11,9 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=skate BUILD_ID=JRO03C BUILD_FINGERPRINT=ZTE/N880E_JB/atlas40:4.1.2/JRO03C/eng.songsy.20120718.233441:eng/test-keys PRIVATE_BUILD_DESC="N880E_JB-eng 4.1.2 JRO03C eng.songsy.20120718.233441 test-keys" BUILD_NUMBER=eng.songsy.20120718.233441
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=skate BUILD_ID=JRO03C BUILD_FINGERPRINT=ZTE/N880E_JB/atlas40:4.1.1/JRO03C/eng.songsy.20120718.233441:eng/test-keys PRIVATE_BUILD_DESC="N880E_JB-eng 4.1.1 JRO03C eng.songsy.20120718.233441 test-keys" BUILD_NUMBER=eng.songsy.20120718.233441
 
-PRODUCT_NAME := pac_skate
+PRODUCT_NAME := cm_skate
 PRODUCT_DEVICE := skate
 PRODUCT_BRAND := ZTE
 PRODUCT_MANUFACTURER := ZTE
